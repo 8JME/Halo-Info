cc526f8065a560d819f89d7d4f475481b0a1f316
J.M <jmoren30@asu.edu>
2020-07-26 20:13:23 -0700

    halofont css tinkering and about page info added

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,22 @@ import '../App.css';
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="container">
+      <h1>Welcome</h1>
+      <hr />
+      <h4>Why this pages exist ...</h4>
+      <br />
+      <p className="paragrap-style">
+      This page was created to fill the void of me not being able to 
+      find information on medals, vehicles, characters, maps, and things of
+      that sort on a single website.
+      </p>
+      <p className="paragrap-style">
+        When there is a void then the need to take up that void was a necessary 
+        and enjoyable taks for me.  I hope to build this site out and excited 
+        what the Halo universe future brings.  I hope this site serves as a handy
+        place to grab Halo gaming information.
+      </p>
     </div>
   );
 }
